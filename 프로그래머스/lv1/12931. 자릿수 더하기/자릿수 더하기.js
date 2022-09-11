88d03914e98a5f4a78de59bd8cf2e4de9c7650ab
Annakim219 <97003692+Annakim219@users.noreply.github.com>
2022-09-11 23:58:14 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.97 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,17 @@
+function solution(n)
+{
+    
+    var answer = 0;
+    
+    let str = n.toString();
+    let str_split = str.split('');
+    
+    for (let i=0; i<str_split.length; i++) {
+        answer += parseInt(str_split[i]);
+    }
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+
+    return answer;
+}
